cf580b4ebca066707516f78d43f34a65816b27de
Victor Quiroz <victorhqc@gmail.com>
2017-12-10 12:37:05 +0100

    Setup react-router

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.getElementById('root'));
+ReactDOM.hydrate(
+  (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  ),
+  document.getElementById('root'),
+);
